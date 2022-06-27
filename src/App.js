20a5dda5282db9087ddf6eb7067980e712e75509
Amol Jangid <amoljangid33@gmail.com>
2022-06-27 12:35:49 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import User from './components/User'
 
 export default function App() {
   return (
     <div>
-      <h1>This is My App Component</h1>
+      <User />
     </div>
   )
 }
