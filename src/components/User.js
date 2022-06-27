97c417abe6256e8d75855facddf7387f6e6660ef
Amol Jangid <amoljangid33@gmail.com>
2022-06-27 15:15:36 +0530

    first commit

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { Button } from '@mui/material'
 export default function User() {
   return (
     <div>
       <h1>This is the User Components</h1>
+      <Button variant="contained">Login</Button>
     </div>
   )
 }
